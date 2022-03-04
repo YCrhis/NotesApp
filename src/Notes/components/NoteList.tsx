e2397e1530d28b12055267bc2a04f2c10a675fe9
YCrhis <htcrhis@gmail.com>
2022-03-03 19:34:58 -0500

    fixing errors

--- src/Notes/components/NoteList.tsx
+++ src/Notes/components/NoteList.tsx
@@ -43,15 +43,6 @@ export const NoteList = () => {
         <div className="notelist">
             <h2>List Of {active}</h2>
             <AnimatePresence>
-                {/* {active === 'All' &&
-                    notes.map(note => <NoteCard key={note.id} note={note} />)
-                }
-                {active === 'Interesting' &&
-                    lisInteresting.map(note => <NoteCard key={note.id} note={note} />)
-                }
-                {active === 'No Interesting' &&
-                    listNotInteresting.map(note => <NoteCard key={note.id} note={note} />)
-                } */}
                 {renderContent()}
             </AnimatePresence>
         </div>
